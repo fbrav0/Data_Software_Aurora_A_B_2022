597d1153b6ddbbe9abb4272bb0e6c2e8f7829430
Mauricio Bedoya <62667433+maurobedoya@users.noreply.github.com>
2022-05-16 18:37:02 -0400

    Refactor extract_frames.py

--- extract_frames.py
+++ extract_frames.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """
+This script will extract pdb files from a clustering output from the tool:
+https://github.com/HITS-MCM/MD-IFP/blob/master/IFP_generation_examples_TRAJ.ipynb
+modified to write the pdb_cluster.csv file. (MODIFIED)
 1. Read the data from pdb_cluster.csv
 2. Iterate over the data
 3. Extract frames from clusters into pdbs
@@ -8,75 +11,10 @@ python extract_ligand_from_clusters_02.py pdb_test.csv -r Dir_Traj1_Metad_0.3.tx
 """
 
 import argparse
-import csv
 import sys
-
-from typing import List, Tuple
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
+from typing import List
 import pandas as pd
-import warnings
-from pathlib import Path
-
-from numpy import arange
-
-import numpy as np
 import MDAnalysis as mda
-
-
-def main(argv: List[str]):
-    opts = parse_args(argv)
-    data = pd.read_csv(opts.input, sep=";", comment="#")
-    paths = pd.read_csv(opts.routes, sep=",", comment="#", quotechar="'")
-    N = int(opts.frames) #Convertir a int
-    print(paths["path"][7])
-    # degree = int(opts.degree)
-    sorted_data = data.sort_values(by="traj")
-    print(sorted_data)
-    trajs = pd.unique(sorted_data["traj"])
-    replicas = pd.unique(sorted_data["replica"])
-    clusters = pd.unique(sorted_data["cluster"])
-
-    i = 0
-    for replica in sorted_data.replica.unique():
-        for traj in sorted_data[sorted_data.replica == replica].traj.unique():
-            print("PRINT",sorted_data[sorted_data.replica == replica].traj.unique())
-            print("LOAD",replica,traj,paths["path"][replica])
-            u = mda.Universe("{}.pdb".format(traj),"{}".format(paths["path"][replica]))
-            u_length = int(len(u.trajectory))
-            #M = sorted_data["frame"][j]
-            #print("MMMM",M)
-            #C = (sorted_data.cluster)
-            #C = pd.unique(sorted_data["cluster"])
-            system_reduced = u.select_atoms("all")
-            #system_reduced.write("{}_{}_{}.pdb".format(C,j,M),frames=u.trajectory[[M]])
-            
-            for row in sorted_data.iterrows():
-                C = row[1]["cluster"]
-                M = row[1]["frame"]
-                for cluster in clusters:
-                    if (
-                        (traj == row[1]["traj"])
-                        and (replica == row[1]["replica"])
-                        and (cluster == row[1]["cluster"])
-                    ):
-                        fr_r=int(u_length-N+M)
-                        print("WRITING",C,replica,M)
-                        system_reduced.write("{}_{}_{}_{}.pdb".format(C,replica,traj,M),frames=u.trajectory[[fr_r]])
-                    
-                #system_reduced.write("{}_{}_{}_{}.pdb".format(C,j,k,M),frames=u.trajectory[[M]])
-    #                #for ts in u.trajectory[0:-300:1]:
-    #                #    print("FRAME",ts.frame)
-            i += 1
-
-
-
-def read_md(reference, traj, replica, frame):
-    for i in range(1, 5):
-        if traj == str("Traj{}_0.6".format(i)):
-            reference = str("{}".format(traj))
 
 
 def parse_args(argv):
@@ -95,10 +33,47 @@ def parse_args(argv):
         "-f",
         dest="frames",
         required=True,
-        help="Frames loaded, for instance, if were loaded the last 300 frames, -f = 300",
+        help="Frames specified in the IFP_generation_examples_TRAJ.ipynb tool.",
     )
     opts = parser.parse_args(argv)
     return opts
+
+
+def main(argv: List[str]):
+    opts = parse_args(argv)
+    data = pd.read_csv(opts.input, sep=";", comment="#")
+    paths = pd.read_csv(opts.routes, sep=",", comment="#", quotechar="'")
+    N = int(opts.frames)  # Convertir a int
+    print(paths["path"][7])
+    sorted_data = data.sort_values(by="traj")
+    print(sorted_data)
+    clusters = pd.unique(sorted_data["cluster"])
+
+    i = 0
+    for replica in sorted_data.replica.unique():
+        for traj in sorted_data[sorted_data.replica == replica].traj.unique():
+            print("PRINT", sorted_data[sorted_data.replica == replica].traj.unique())
+            print("LOAD", replica, traj, paths["path"][replica])
+            u = mda.Universe("{}.pdb".format(traj), "{}".format(paths["path"][replica]))
+            u_length = int(len(u.trajectory))
+            system_reduced = u.select_atoms("all")
+
+            for row in sorted_data.iterrows():
+                C = row[1]["cluster"]
+                M = row[1]["frame"]
+                for cluster in clusters:
+                    if (
+                        (traj == row[1]["traj"])
+                        and (replica == row[1]["replica"])
+                        and (cluster == row[1]["cluster"])
+                    ):
+                        fr_r = int(u_length - N + M)
+                        print("WRITING", C, replica, M)
+                        system_reduced.write(
+                            "{}_{}_{}_{}.pdb".format(C, replica, traj, M),
+                            frames=u.trajectory[[fr_r]],
+                        )
+            i += 1
 
 
 if __name__ == "__main__":
